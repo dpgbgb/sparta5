dbe4cd91608820d46741259bf284484eff9cd75b
dpgbgb <ekdqbs@gmail.com>
2025-01-22 01:14:29 +0900

    Update data.py

--- data.py
+++ data.py
@@ -3,25 +3,25 @@ from google.colab import userdata
 import xml.etree.cElementTree as ET
 import pandas as pd
 
-def data():
-    url = 'https://openapi.gg.go.kr/ANIMALREGSTUS'
-    
-    params = {
-        ('Key', userdata.get('sampleKey')),
-        ('Type','xml'),
-        ('plndex',1),
-        ('pSize', 100)
-    }
-    
-    a = requests.get(url, params)
-    root = ET.fromstring(a.text)
-    
-    p=[]
-    v= {}
-    
-    for i in root.findall('row'):
-      for j in i:
-        v[j.tag] = j.text
-      p.append(v)
-    
-   return pd.DataFrame(p)
+url = 'https://openapi.gg.go.kr/ANIMALREGSTUS'
+
+params = {
+    ('Key', userdata.get('sampleKey')),
+    ('Type','xml'),
+    ('plndex',1),
+    ('pSize', 100)
+}
+
+a = requests.get(url, params)
+root = ET.fromstring(a.text)
+
+p=[]
+v= {}
+
+for i in root.findall('row'):
+  for j in i:
+    v[j.tag] = j.text
+  p.append(v)
+
+df = pd.DataFrame(p)
+df
